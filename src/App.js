2484e6dad852e0a165fbcc050b10d4b0efac87c2
Paul Brislane <paul.brislane@onthebeach.co.uk>
2019-10-11 15:29:13 +0100

    Googletime

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import './App.scss';
+import React from "react";
+import "./App.scss";
 
 function App() {
   return (
@@ -54,9 +54,16 @@ function App() {
         </div>
       </section>
       <section className="body">
+        <div className="body__trunk"></div>
+        <div className="body__tear"></div>
+        <div className="body__tear1"></div>
+        <div className="body__tear2"></div>
+        <div className="body__tear3"></div>
         <div className="body__chest"></div>
         <div className="body__front"></div>
         <div className="body__back"></div>
+        <div className="body__tear4"></div>
+        <div className="body__tear5"></div>
       </section>
       <section className="arm-right">
         <div className="arm-right__upper"></div>
@@ -98,19 +105,16 @@ function App() {
 
       <section className="feet">
         <div className="feet__left"></div>
-        <div className="feet__left-heel"></div>
-        <div className="feet__left-toe-box"></div>
-        <div className="feet__left-little-toe"></div>
-        <div className="feet__left-toe-2"></div>
-        <div className="feet__left-toe-3"></div>
-        <div className="feet__left-big-toe"></div>
-
         <div className="feet__right"></div>
+        <div className="feet__left-heel"></div>
         <div className="feet__right-heel"></div>
-        <div className="feet__right-toe-box"></div>
+        <div className="feet__left-little-toe"></div>
         <div className="feet__right-little-toe"></div>
+        <div className="feet__left-toe-2"></div>
         <div className="feet__right-toe-2"></div>
+        <div className="feet__left-toe-3"></div>
         <div className="feet__right-toe-3"></div>
+        <div className="feet__left-big-toe"></div>
         <div className="feet__right-big-toe"></div>
       </section>
     </main>
